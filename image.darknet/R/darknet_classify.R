c74a96b923044d919d70877d8f30f1093b518163
Jan Wijffels <jwijffels@bnosac.be>
2017-02-28 17:37:26 +0100

    Split out models in different file

--- image.darknet/R/darknet_classify.R
+++ image.darknet/R/darknet_classify.R
@@ -45,6 +45,7 @@
 #' }
 image_darknet_classify <- function(file, object, top=5L) {
   stopifnot(file.exists(file))
+  stopifnot(object$type == "classify")
   if(any(basename(file) == file)){
     stop("Give a full path to the file instead of a path inside the working directory")
   }
@@ -53,121 +54,4 @@ image_darknet_classify <- function(file, object, top=5L) {
                   file, top, object$labels, as.integer(object$resize), PACKAGE = "image.darknet")
   list(file = file, 
        type = data.frame(label = result[[2]], probability = result[[1]], stringsAsFactors = FALSE))
-}
-
-
-
-#' @title Specify a model to be used in classification or object detection
-#' @description Specify a model to be used in classification or object detection.
-#' For classification this consists of
-#' \itemize{
-#'  \item{model: }{the deep learning configuration file which was used to train the model}
-#'  \item{weights: }{the trained weights of the deep learning model}
-#'  \item{labels: }{a character vector of possible labels to classify an image - used during training}
-#' }
-#' @param type character string, either 'classify' for classification or 'detect' for object detection
-#' @param model character string with the full path to the deep learning configuration file
-#' @param weights character string with the full path to the trained deep learning weights
-#' @param labels character vector of labels
-#' @param resize logical indicating to resize the network. Defaults to TRUE. Set to FALSE for the Alexnet and VGG-16 model
-#' @return an object of class darknet_model which is a list with these files
-#' @export
-#' @examples
-#' ##
-#' ## Define the model (structure of the deep learning model + the learned weights + the labels)
-#' ##
-#' model <- system.file(package="image.darknet", "include", "darknet", "cfg", "tiny.cfg")
-#' weights <- system.file(package="image.darknet", "models", "tiny.weights")
-#' f <- system.file(package="image.darknet", "include", "darknet", "data", "imagenet.shortnames.list")
-#' labels <- readLines(f)
-#' 
-#' darknet_tiny <- image_darknet_model(type = 'classify', 
-#'                                     model = model, weights = weights, labels = labels)
-#' darknet_tiny
-#' 
-#' ##
-#' ## Other models which are trained already can be downloaded at: 
-#' ## https://pjreddie.com/darknet/imagenet
-#' ##
-#' \dontrun{
-#' ## AlexNet
-#' weights <- file.path(system.file(package="image.darknet", "models"), "alexnet.weights")
-#' download.file(url = "http://pjreddie.com/media/files/alexnet.weights", destfile = weights)
-#' model <- system.file(package="image.darknet", "include", "darknet", "cfg", "alexnet.cfg")
-#' 
-#' alexnet <- image_darknet_model(type = 'classify', 
-#'                                model = model, weights = weights, labels = labels, resize=FALSE)
-#' alexnet
-#' 
-#' ## Darknet Reference
-#' weights <- file.path(system.file(package="image.darknet", "models"), "darknet.weights")
-#' download.file(url = "http://pjreddie.com/media/files/darknet.weights", destfile = weights)
-#' model <- system.file(package="image.darknet", "include", "darknet", "cfg", "darknet.cfg")
-#' 
-#' darknetref <- image_darknet_model(type = 'classify', 
-#'                                   model = model, weights = weights, labels = labels)
-#' darknetref
-#' 
-#' ## vgg-16
-#' weights <- file.path(system.file(package="image.darknet", "models"), "vgg-16.weights")
-#' download.file(url = "http://pjreddie.com/media/files/vgg-16.weights", destfile = weights)
-#' model <- system.file(package="image.darknet", "include", "darknet", "cfg", "vgg-16.cfg")
-#' 
-#' vgg16 <- image_darknet_model(type = 'classify', 
-#'                              model = model, weights = weights, labels = labels, resize=FALSE)
-#' vgg16
-#' 
-#' ## googlenet
-#' weights <- file.path(system.file(package="image.darknet", "models"), "extraction.weights")
-#' download.file(url = "http://pjreddie.com/media/files/extraction.weights", destfile = weights)
-#' model <- system.file(package="image.darknet", "include", "darknet", "cfg", "extraction.cfg")
-#' 
-#' googlenet <- image_darknet_model(type = 'classify', 
-#'                                  model = model, weights = weights, labels = labels)
-#' googlenet
-#' 
-#' ## darknet19
-#' weights <- file.path(system.file(package="image.darknet", "models"), "darknet19.weights")
-#' download.file(url = "http://pjreddie.com/media/files/darknet19.weights", destfile = weights)
-#' model <- system.file(package="image.darknet", "include", "darknet", "cfg", "darknet19.cfg")
-#' 
-#' darknet19 <- image_darknet_model(type = 'classify', 
-#'                                  model = model, weights = weights, labels = labels)
-#' darknet19
-#' 
-#' ## Darknet19 448x448
-#' weights <- file.path(system.file(package="image.darknet", "models"), "darknet19_448.weights")
-#' download.file(url = "http://pjreddie.com/media/files/darknet19_448.weights", destfile = weights)
-#' model <- system.file(package="image.darknet", "include", "darknet", "cfg", "darknet19_448.cfg")
-#' 
-#' darknet_19 <- image_darknet_model(type = 'classify', 
-#'                                   model = model, weights = weights, labels = labels)
-#' darknet_19
-#' }
-image_darknet_model <- function(type = c("classify", "detect"), model, weights, labels, resize=TRUE){
-  stopifnot(file.exists(model))
-  stopifnot(file.exists(weights))
-  stopifnot(is.character(labels) & length(labels) > 1)
-  type <- match.arg(type)
-  out <- list()
-  out$type <- type
-  if(type == "classify"){
-    out$datacfg <- "not_needed_any_more.data"
-    out$cfgfile  <- model
-    out$weightfile <- weights
-    out$labels <- labels
-    out$resize <- resize
-  }
-  class(out) <- "darknet_model"
-  out
-}
-
-
-#' @export
-print.darknet_model <- function(x, ...){
-  cat("Darknet model", sep = "\n")
-  cat(sprintf("  number of labels: %s", length(x$labels)), sep = "\n")
-  cat(sprintf("  example labels: %s", paste(head(x$labels, 10), collapse = ", ")), sep = "\n\n")
-  cat("Darknet model structure", sep = "\n\n")   
-  cat(readLines(x$cfgfile), sep = "\n")   
 }
---
+++ image.darknet/R/darknet_models.R
@@ -0,0 +1,177 @@
+#' @title Specify a model to be used in classification or object detection
+#' @description Specify a model to be used in classification or object detection.
+#' For classification this consists of
+#' \itemize{
+#'  \item{model: }{the deep learning configuration file which was used to train the model}
+#'  \item{weights: }{the trained weights of the deep learning model}
+#'  \item{labels: }{a character vector of possible labels to classify an image - used during training}
+#' }
+#' Available models are 
+#' \itemize{
+#'  \item{classification: }{Alexnet, Darknet, VGG-16, Extraction(GoogleNet), Darknet19, Darknet19_448, Tiny Darknet trained on Imagenet}
+#'  \item{detection: }{YOLO, YOLO tiny trained on VOC and on COCO}
+#' }
+#' @param type character string, either 'classify' for classification or 'detect' for object detection
+#' @param model character string with the full path to the deep learning configuration file or
+#' one of the following preset models: classification: tiny.cfg, alexnet.cfg, darknet.cfg, vgg-16.cfg, 
+#' extraction.cfg, darknet19.cfg, darknet19_448.cfg which are available in the includ/darket/cfg folder
+#' of this package
+#' @param weights character string with the full path to the trained deep learning weights
+#' @param labels character vector of labels
+#' @param resize logical indicating to resize the network if type is 'classify'. 
+#' Defaults to TRUE. Set to FALSE for the Alexnet and VGG-16 model
+#' @return an object of class darknet_model which is a list with these files
+#' @export
+#' @examples
+#' ##
+#' ## Define the classification model 
+#' ## (structure of the deep learning model + the learned weights + the labels)
+#' ##
+#' model <- system.file(package="image.darknet", "include", "darknet", "cfg", )
+#' weights <- system.file(package="image.darknet", "models", "tiny.weights")
+#' f <- system.file(package="image.darknet", "include", "darknet", "data", "imagenet.shortnames.list")
+#' labels <- readLines(f)
+#' darknet_tiny <- image_darknet_model(type = 'classify', 
+#'                                     model = "tiny.cfg", weights = weights, labels = labels)
+#' darknet_tiny
+#' \dontrun{
+#' ##
+#' ## Other CLASSIFICATION models which are trained already can be downloaded at: 
+#' ## https://pjreddie.com/darknet/imagenet
+#' ##
+#' 
+#' ## AlexNet
+#' weights <- file.path(system.file(package="image.darknet", "models"), "alexnet.weights")
+#' download.file(url = "http://pjreddie.com/media/files/alexnet.weights", destfile = weights)
+#' alexnet <- image_darknet_model(type = 'classify', 
+#'   model = "alexnet.cfg", weights = weights, labels = labels, resize=FALSE)
+#' alexnet
+#' 
+#' ## Darknet Reference
+#' weights <- file.path(system.file(package="image.darknet", "models"), "darknet.weights")
+#' download.file(url = "http://pjreddie.com/media/files/darknet.weights", destfile = weights)
+#' darknetref <- image_darknet_model(type = 'classify', 
+#'   model = "darknet.cfg", weights = weights, labels = labels)
+#' darknetref
+#' 
+#' ## vgg-16
+#' weights <- file.path(system.file(package="image.darknet", "models"), "vgg-16.weights")
+#' download.file(url = "http://pjreddie.com/media/files/vgg-16.weights", destfile = weights)
+#' vgg16 <- image_darknet_model(type = 'classify', 
+#'   model = "vgg-16.cfg", weights = weights, labels = labels, resize=FALSE)
+#' vgg16
+#' 
+#' ## googlenet/extraction
+#' weights <- file.path(system.file(package="image.darknet", "models"), "extraction.weights")
+#' download.file(url = "http://pjreddie.com/media/files/extraction.weights", destfile = weights)
+#' googlenet <- image_darknet_model(type = 'classify', 
+#'   model = "extraction.cfg", weights = weights, labels = labels)
+#' googlenet
+#' 
+#' ## darknet19
+#' weights <- file.path(system.file(package="image.darknet", "models"), "darknet19.weights")
+#' download.file(url = "http://pjreddie.com/media/files/darknet19.weights", destfile = weights)
+#' darknet19 <- image_darknet_model(type = 'classify', 
+#'   model = "darknet19.cfg", weights = weights, labels = labels)
+#' darknet19
+#' 
+#' ## Darknet19 448x448
+#' weights <- file.path(system.file(package="image.darknet", "models"), "darknet19_448.weights")
+#' download.file(url = "http://pjreddie.com/media/files/darknet19_448.weights", destfile = weights)
+#' darknet_19 <- image_darknet_model(type = 'classify', 
+#'   model = "darknet19_448.cfg", weights = weights, labels = labels)
+#' darknet_19
+#' }
+#' 
+#' 
+#' http://pjreddie.com/media/files/
+#' 
+#' ##
+#' ## Define the detection model (YOLO) 
+#' ## (structure of the deep learning model + the learned weights + the labels)
+#' ##
+#' f <- system.file(package="image.darknet", "include", "darknet", "data", "voc.names")
+#' labels <- readLines(f)
+#' 
+#' yolo_tiny_voc <- image_darknet_model(type = 'detect', 
+#'  model = "tiny-yolo-voc.cfg", 
+#'  weights = system.file(package="image.darknet", "models", "tiny-yolo-voc.weights"), 
+#'  labels = labels)
+#' yolo_tiny_voc
+#' 
+#' \dontrun{
+#' ##
+#' ## Other DETECTION models which are trained already can be downloaded at: 
+#' ## https://pjreddie.com/darknet/yolo/
+#' ##
+#' weights <- file.path(system.file(package="image.darknet", "models"), "yolo-voc.weights")
+#' download.file(url = "http://pjreddie.com/media/files/yolo-voc.weights", destfile = weights)
+#' yolo_voc <- image_darknet_model(type = 'detect', 
+#'  model = "yolo-voc.cfg", 
+#'  weights = system.file(package="image.darknet", "models", "yolo-voc.weights"), 
+#'  labels = labels)
+#' yolo_voc
+#' 
+#' 
+#' ## trained on COCO
+#' f <- system.file(package="image.darknet", "include", "darknet", "data", "coco.names")
+#' labels <- readLines(f)
+#' 
+#' weights <- file.path(system.file(package="image.darknet", "models"), "tiny-yolo.weights")
+#' download.file(url = "http://pjreddie.com/media/files/tiny-yolo.weights", destfile = weights)
+#' yolo_tiny_coco <- image_darknet_model(type = 'detect', 
+#'  model = "tiny-yolo.cfg", 
+#'  weights = system.file(package="image.darknet", "models", "tiny-yolo.weights"), 
+#'  labels = labels)
+#' yolo_tiny_coco
+#' 
+#' weights <- file.path(system.file(package="image.darknet", "models"), "yolo.weights")
+#' download.file(url = "http://pjreddie.com/media/files/yolo.weights", destfile = weights)
+#' yolo_coco <- image_darknet_model(type = 'detect', 
+#'  model = "yolo.cfg", 
+#'  weights = system.file(package="image.darknet", "models", "yolo.weights"), 
+#'  labels = labels)
+#' yolo_coco
+#' }
+image_darknet_model <- function(type = c("classify", "detect"), model, weights, labels, resize=TRUE){
+  if(model %in% c("tiny.cfg", "alexnet.cfg", "darknet.cfg", "vgg-16.cfg", 
+                  "extraction.cfg", "darknet19.cfg", "darknet19_448.cfg",
+                  "yolo.cfg", "tiny-yolo.cfg", "yolo-voc", "tiny-yolo-voc.cfg")){
+    model <- system.file(package="image.darknet", "include", "darknet", "cfg", model)
+  }
+  stopifnot(file.exists(model))
+  stopifnot(file.exists(weights))
+  stopifnot(is.character(labels))
+  if(length(labels) == 1){
+    stopifnot(file.exists(labels))
+    labels <- readLines(labels)
+    stopifnot(length(labels) > 1)
+  }
+  type <- match.arg(type)
+  out <- list()
+  out$type <- type
+  if(type == "classify"){
+    out$datacfg <- "not_needed_any_more.data"
+    out$cfgfile  <- model
+    out$weightfile <- weights
+    out$labels <- labels
+    out$resize <- resize
+  }else if(type == "detect"){
+    out$datacfg <- "not_needed_any_more.data"
+    out$cfgfile  <- model
+    out$weightfile <- weights
+    out$labels <- labels
+  }
+  class(out) <- "darknet_model"
+  out
+}
+
+
+#' @export
+print.darknet_model <- function(x, ...){
+  cat("Darknet model", sep = "\n")
+  cat(sprintf("  number of labels: %s", length(x$labels)), sep = "\n")
+  cat(sprintf("  example labels: %s", paste(head(x$labels, 10), collapse = ", ")), sep = "\n\n")
+  cat("Darknet model structure", sep = "\n\n")   
+  cat(readLines(x$cfgfile), sep = "\n")   
+}
